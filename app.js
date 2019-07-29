c3d85cdfe5a011e98c4dc193dbf617126083fc3e
alinetskyi <artem.linetskiy@gmail.com>
2019-07-29 12:30:45 +0300

    Integrate JWT module and clean up main app file

--- app.js
+++ app.js
@@ -1,74 +1,29 @@
 const express=require('express');
-const jwt = require("jsonwebtoken");
-const fs = require('fs')
-
-// const jwt=require('./jwt.js');
-
-const config = require('./config/config.js');
+const jwt = require('./jwt.js');
 const request = require('request')
 const app=express();
-var applogic_private_key = null;
-
-try {
-  applogic_private_key = fs.readFileSync(String(global.gConfig.jwt.private_key_path), 'utf8')
-} catch (err) {
-  console.error(err)
-}
+const config = require('./config/config.js');
 
 app.use(require('body-parser').json());
-app.use(function(req,res,next){
-    try{
-    const token = req.headers.authorization.split(" ")[1]
-    jwt.verify(token, key.tokenKey, function (err, payload) {
-        console.log(payload)
-        if (payload) {
-            user.findById(payload.userId).then(
-                (doc)=>{
-                    req.user=doc;
-                    next()
-                }
-            )
-        } else {
-           next()
-        }
-    })
-}catch(e){
-    next()
-}
+
+app.listen(global.gConfig.node_port || process.env.PORT,()=>{
+  console.log(`${global.gConfig.app_name} listening on port ${global.gConfig.node_port}`);
 })
-app.get('/api/users/get',function(req,res){
-  var decoded_key = new Buffer(applogic_private_key.trim(), 'base64').toString('utf-8')
-  var payload = Buffer.from(JSON.stringify({ email:"admin@barong.io"})).toString('base64')
-  signed_payload = jwt.sign(
-    payload,
-    decoded_key,
-    { algorithm: global.gConfig.jwt.algorithm })
-    console.log("SIGNED PAYLOAD:", signed_payload)
-  protected = signed_payload.split('.')[0]
-  signature = signed_payload.split('.')[2]
-  request_params = {
-    email: "admin@barong.io",
-    payload: payload,
-    signatures: [{
-    protected: protected,
-    header: { kid: "applogic" },
-    signature: signature
-    }]
-  }
-    console.log("PAYLOAD:", payload)
-    console.log("REQUST_PARAMS:", request_params)
+
+app.post('/api/users/get',function(req,res){
+  payload = { email: req.body.email }
+  signed_payload = jwt.sign(payload)
+  request_params = jwt.formatParams(signed_payload)
   request({
     method: "POST",
     uri: global.gConfig.barong_url,
     json: true,
     body: request_params }, (err, res, body) => {
       console.log(body)
-      if (err) { return console.log(err);
+      if (err) { return console.error(err);
     }
-});
+  });
+  console.log("REQUEST PARAMS:",request_params)
   res.json(global.gConfig);
 })
 
-app.listen(global.gConfig.node_port || process.env.PORT,()=>{
-  console.log(`${global.gConfig.app_name} listening on port ${global.gConfig.node_port}`);
-})
--- jwt.js
+++ jwt.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const config = require('./config/config.js');
 const fs = require('fs')
-const decoded_key = new Buffer(global.gConfig.jwt.private_key.trim(), 'base64').toString('utf-8')
+const decoded_key = Buffer.from(global.gConfig.jwt.private_key.trim(), 'base64').toString('utf-8')
 
 module.exports = {
   /**
@@ -10,34 +10,15 @@ module.exports = {
    * @param {Object} payload  the payload send to the user
    * @param {Function} callback function envoked with two parameters
    *                        1. error the error in operation
-   *                        2. token the signed payload
-   * @returns {Promise}
+   * n   2. token the signed payload
    */
-  sign(payload, callback) {
-    return new Promise((resolve, reject) => {
-      jwt.sign(
-        { payload: payload, exp: Date.now() + global.gConfig.jwt.expire_date },
-        decoded_key,
-        { algorithm: global.gConfig.jwt.algorithm },
-        function(errorInSign, token) {
-          if (errorInSign) {
-            if (callback) {
-              callback(errorInSign.message);
-              resolve(null);
-            } else {
-              reject(errorInSign.message);
-            }
-          } else {
-            if (callback) {
-              callback(null, token);
-              resolve(null);
-            } else {
-              resolve(token);
-            }
-          }
-        }
-      );
-    });
+  sign (payload, callback) {
+    token = jwt.sign(
+      { data: payload, exp: Date.now() + global.gConfig.jwt.expire_date },
+      decoded_key,
+      { algorithm: global.gConfig.jwt.algorithm }
+    )
+    return token
   },
 
   /**
@@ -47,30 +28,28 @@ module.exports = {
    * @param {Function} callback function envoked on completion of the task with
    *                        1. error the error in verification
    *                        2. if verification is successfull the decoded data is passed.
-   * @returns {Promise}
    */
   verify(token, callback) {
-    return new Promise((resolve, reject) => {
-      jwt.verify(token, decoded_key, function(
-        errorInVerfication,
-        decodedData
-      ) {
-        if (errorInVerfication) {
-          if (callback) {
-            callback(errorInVerfication.message);
-            resolve(null);
-          } else {
-            reject(errorInVerfication.message);
-          }
-        } else {
-          if (callback) {
-            callback(null, decodedData);
-            resolve(null);
-          } else {
-            resolve(decodedData);
-          }
-        }
-      });
-    });
+      result = jwt.verify(token, decoded_key)
+      return result
+  },
+
+   /**
+   * formatParams - format request parameters.
+   *
+   * @param {Oblect} payload    payload to format
+   */
+
+  formatParams(payload) {
+    request_params = {
+      payload: payload.split('.')[1],
+      signatures: [{
+      protected: payload.split('.')[0],
+      header: { kid: 'applogic' },
+      signature: payload.split('.')[2]
+      }]
+    }
+    return request_params
   }
+
 };
