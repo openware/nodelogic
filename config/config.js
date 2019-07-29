6e8f77a2223b26ffb8723e707fee567b18090d51
alinetskyi <artem.linetskiy@gmail.com>
2019-07-29 13:26:52 +0300

    Reorganise configuration and move it to env's

--- config/config.js
+++ config/config.js
@@ -8,8 +8,15 @@ const config = require('./config.json');
 const defaultConfig = config.development;
 const environment = process.env.NODE_ENV || 'development';
 const environmentConfig = config[environment];
-const finalConfig = _.merge(defaultConfig, environmentConfig);
 
+// set up config from env's
+environmentConfig.barong_url = process.env.BARONG_URL || 'http://localhost:3000/api/v2/management/users/get';
+environmentConfig.jwt.expire_date = process.env.JWT_EXPIRE_DATE || 60;
+environmentConfig.jwt.algorithm = process.env.JWT_ALGORITHM || 'RS256';
+environmentConfig.jwt.private_key = process.env.JWT_PRIVATE_KEY;
+environmentConfig.node_port = process.env.PORT || defaultConfig.node_port;
+
+const finalConfig = _.merge(defaultConfig, environmentConfig);
 // as a best practice
 // all global variables should be referenced via global. syntax
 // and their names should always begin with g
