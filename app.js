254bfa1ba3707732481323b6ac80b26cffb2d6a9
alinetskyi <artem.linetskiy@gmail.com>
2019-07-29 12:38:01 +0300

    Beautify repository code

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-const express=require('express');
+const express = require('express');
 const jwt = require('./jwt.js');
 const request = require('request')
-const app=express();
+const app = express();
 const config = require('./config/config.js');
 
 app.use(require('body-parser').json());
 
-app.listen(global.gConfig.node_port || process.env.PORT,()=>{
-  console.log(`${global.gConfig.app_name} listening on port ${global.gConfig.node_port}`);
+app.listen(global.gConfig.node_port || process.env.PORT, () => {
+    console.log(`${global.gConfig.app_name} listening on port ${global.gConfig.node_port}`);
 })
 
-app.post('/api/users/get',function(req,res){
-  payload = { email: req.body.email }
-  signed_payload = jwt.sign(payload)
-  request_params = jwt.formatParams(signed_payload)
-  request({
-    method: "POST",
-    uri: global.gConfig.barong_url,
-    json: true,
-    body: request_params }, (err, res, body) => {
-      console.log(body)
-      if (err) { return console.error(err);
+app.post('/api/users/get', function(req, res) {
+    payload = {
+        email: req.body.email
     }
-  });
-  console.log("REQUEST PARAMS:",request_params)
-  res.json(global.gConfig);
+    signed_payload = jwt.sign(payload)
+    request_params = jwt.formatParams(signed_payload)
+    request({
+        method: "POST",
+        uri: global.gConfig.barong_url,
+        json: true,
+        body: request_params
+    }, (err, res, body) => {
+        console.log(body)
+        if (err) {
+            return console.error(err);
+        }
+    });
+    console.log("REQUEST PARAMS:", request_params)
+    res.json(global.gConfig);
 })
-
--- jwt.js
+++ jwt.js
@@ -4,52 +4,57 @@ const fs = require('fs')
 const decoded_key = Buffer.from(global.gConfig.jwt.private_key.trim(), 'base64').toString('utf-8')
 
 module.exports = {
-  /**
-   * sign - create a jwt from payload.
-   *
-   * @param {Object} payload  the payload send to the user
-   * @param {Function} callback function envoked with two parameters
-   *                        1. error the error in operation
-   * n   2. token the signed payload
-   */
-  sign (payload, callback) {
-    token = jwt.sign(
-      { data: payload, exp: Date.now() + global.gConfig.jwt.expire_date },
-      decoded_key,
-      { algorithm: global.gConfig.jwt.algorithm }
-    )
-    return token
-  },
+    /**
+     * sign - create a jwt from payload.
+     *
+     * @param {Object} payload  the payload send to the user
+     * @param {Function} callback function envoked with two parameters
+     *                        1. error the error in operation
+     * n   2. token the signed payload
+     */
+    sign(payload, callback) {
+        token = jwt.sign({
+                data: payload,
+                exp: Date.now() + global.gConfig.jwt.expire_date
+            },
+            decoded_key, {
+                algorithm: global.gConfig.jwt.algorithm
+            }
+        )
+        return token
+    },
 
-  /**
-   * verify - check the token signature.
-   *
-   * @param {String} token    the token for verification
-   * @param {Function} callback function envoked on completion of the task with
-   *                        1. error the error in verification
-   *                        2. if verification is successfull the decoded data is passed.
-   */
-  verify(token, callback) {
-      result = jwt.verify(token, decoded_key)
-      return result
-  },
+    /**
+     * verify - check the token signature.
+     *
+     * @param {String} token    the token for verification
+     * @param {Function} callback function envoked on completion of the task with
+     *                        1. error the error in verification
+     *                        2. if verification is successfull the decoded data is passed.
+     */
+    verify(token, callback) {
+        result = jwt.verify(token, decoded_key)
+        return result
+    },
 
-   /**
-   * formatParams - format request parameters.
-   *
-   * @param {Oblect} payload    payload to format
-   */
+    /**
+     * formatParams - format request parameters.
+     *
+     * @param {Oblect} payload    payload to format
+     */
 
-  formatParams(payload) {
-    request_params = {
-      payload: payload.split('.')[1],
-      signatures: [{
-      protected: payload.split('.')[0],
-      header: { kid: 'applogic' },
-      signature: payload.split('.')[2]
-      }]
+    formatParams(payload) {
+        request_params = {
+            payload: payload.split('.')[1],
+            signatures: [{
+                protected: payload.split('.')[0],
+                header: {
+                    kid: 'applogic'
+                },
+                signature: payload.split('.')[2]
+            }]
+        }
+        return request_params
     }
-    return request_params
-  }
 
 };
