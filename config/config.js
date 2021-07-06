a961a95b37b4972adda58d0af770ead09306a9b0
josadcha <josadcha@heliostech.fr>
2021-07-06 17:19:15 +0300

    Enhancement: update barong-auth, add peatio_url

--- config/config.js
+++ config/config.js
@@ -13,6 +13,7 @@ const environmentConfig = config[environment];
 environmentConfig.barong_jwt_public_key =
   process.env.BARONG_JWT_PUBLIC_KEY || defaultConfig.barong_jwt_public_key;
 environmentConfig.barong_url = process.env.BARONG_URL || defaultConfig.barong_url;
+environmentConfig.peatio_url = process.env.HOSTNAME || defaultConfig.peatio_url;
 environmentConfig.jwt.expire_date = process.env.JWT_EXPIRE_DATE || 60;
 environmentConfig.jwt.algorithm = process.env.JWT_ALGORITHM || 'RS256';
 environmentConfig.jwt.private_key = process.env.JWT_PRIVATE_KEY || defaultConfig.jwt.private_key;
